4f599615af6ccfdb189d1dc5ee215e38772e5483
Mercédesz Papp <mercedesz.papp@stud.uni-corvinus.hu>
2022-09-30 13:05:31 +0200

    File write

--- UserMaintenance_PAY3AU/UserMaintenance_PAY3AU/Form1.cs
+++ UserMaintenance_PAY3AU/UserMaintenance_PAY3AU/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,33 @@ namespace UserMaintenance_PAY3AU
             InitializeComponent();
             lastNamelb.Text = Resource1.FullName;
             btnadd.Text = Resource1.Add;
+            btnFile.Text = Resource1.File;
             listUsers.DataSource = users;
-            listUsers.DataSource = users;
+            listUsers.ValueMember= "ID";
             listUsers.DisplayMember = "FullName";
+           
+
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.Default))
+            {
+                foreach (var u in users)
+                {
+                    sw.WriteLine($"{u.FullName};{u.ID}");
+                }
+            }
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
             var u = new User()
             {
                 FullName = textLastName.Text
