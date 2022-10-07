3f4f15110190ddff5ac345bb83290de62a5f2e1b
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-10-07 23:45:06 +0200

    Header fotmat

--- negyedik_GYAK/negyedik_GYAK/Form1.cs
+++ negyedik_GYAK/negyedik_GYAK/Form1.cs
@@ -81,6 +81,14 @@ namespace negyedik_GYAK
 
 
             }
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
             object[,] values = new object[Flats.Count, headers.Length];
             int counter = 0;
             foreach (Flat f in Flats)
