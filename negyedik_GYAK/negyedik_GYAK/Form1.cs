ae85bcde57f2380b35c05973a8c1ececbaafd77e
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-10-07 23:43:27 +0200

    Write Flats in Excel

--- negyedik_GYAK/negyedik_GYAK/Form1.cs
+++ negyedik_GYAK/negyedik_GYAK/Form1.cs
@@ -38,7 +38,7 @@ namespace negyedik_GYAK
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                CreateTable(); Ennek megírása a következő feladatrészben következik
+                CreateTable(); 
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -55,6 +55,8 @@ namespace negyedik_GYAK
                 xlWB = null;
                 xlApp = null;
             }
+           
+           
 
         }
 
@@ -79,13 +81,54 @@ namespace negyedik_GYAK
 
 
             }
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                
 
+
+
+;
+                counter++;
+            }
+            xlSheet.get_Range(
+              GetCell(2, 1),
+              GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
 
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
 
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+         
     }
+    
+
+
+
 }
