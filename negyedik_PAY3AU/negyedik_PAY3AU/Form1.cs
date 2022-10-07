38e9ef8babc0c362170f70edbd4ce0d080ebab14
Mercédesz Papp <mercedesz.papp@stud.uni-corvinus.hu>
2022-10-07 12:31:33 +0200

    Load data

--- negyedik_PAY3AU/negyedik_PAY3AU/Form1.cs
+++ negyedik_PAY3AU/negyedik_PAY3AU/Form1.cs
@@ -12,9 +12,18 @@ namespace negyedik_PAY3AU
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
