4bde200a7251bcf0dc79012f4f922c8697b3a734
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-10-21 10:29:32 +0200

    Get exchange rates

--- otodik_PAY3AU/otodik_PAY3AU/Form1.cs
+++ otodik_PAY3AU/otodik_PAY3AU/Form1.cs
@@ -15,6 +15,25 @@ namespace otodik_PAY3AU
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+            
+        }
+
+        public void GetExchangeRates()
+        {
+            var mnbService = new MnbServiceReference.MNBArfolyamServiceSoapClient();
+            var request = new MnbServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+            
+
+
+
         }
     }
 }
