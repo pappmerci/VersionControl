d5d17b96d78cb9d4bea5b896ef69aeaaead6ff0f
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-10-26 18:21:21 +0200

    Create ball factory

---
+++ 6Het_PAY3AU/6Het_PAY3AU/Entities/BallFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6Het_PAY3AU.Entities
+{
+   public class BallFactory
+    { public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
