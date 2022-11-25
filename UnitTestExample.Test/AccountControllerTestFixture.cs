cfd1ead63c4e85fce40ed474d8e19c766b52aa3f
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-11-25 12:37:13 +0100

    Email validate test

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,15 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
+
+
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            // ...
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
