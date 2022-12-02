c21f3535fb6c16d69ba448b17f68894f4b710c3e
pappmerci <114682892+pappmerci@users.noreply.github.com>
2022-12-02 12:03:52 +0100

    Create classes

---
+++ 11het_PAY3AU/11het_PAY3AU/Entities/Other/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11het_PAY3AU.Entities.Other
+{
+    public class BirthProbability
+    {
+        public double P { get; set; }
+        public int kor { get; set; }
+        public int  gyerekek_szama{ get; set; }
+
+    }
+}
---
+++ 11het_PAY3AU/11het_PAY3AU/Entities/Other/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11het_PAY3AU.Entities.Other
+{
+    public class DeathProbability
+    {
+        public int kor { get; set; }
+        public string nem { get; set; }
+        public double P{ get; set; }
+
+    }
+}
